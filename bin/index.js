6d11cd17166762e7ad169f9acf6f79df946f2eaa
James Hunt <10615884+thetwopct@users.noreply.github.com>
2025-08-14 19:46:51 +0700

    Improve sluggification of namespace and title, closes #12

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
+const pkg = require("../package.json");
+
+const args = process.argv.slice(2);
+
+if (args.includes("--version") || args.includes("-v")) {
+    console.log(pkg.version);
+    process.exit(0);
+}
+
 const create = require("../lib/create-acf-block-json");
 
 create.acfBlock();
